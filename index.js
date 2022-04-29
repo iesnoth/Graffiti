5065d462a1fbf7a0c0a749e5b22921d67e4fff0a
Victoria Beasley <iesnoth91@gmail.com>
2022-04-28 21:32:15 -0500

    did graffiti

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Select some elements...
+let header = document.querySelector("#page-header")
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll(".dog-image")
+for (let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px';
+    dogImages[i].style.transform = 'rotate(180deg)'
+
+}
+
+let dogName = document.querySelectorAll(".dog-name")
+for (let i = 0; i < dogName.length; i++){
+    dogName[i].style.transform= 'rotate(180deg)'
+}
+
+let footer = document.querySelector(".footer")
+footer.style.color = 'red'
+
+let dogBio = document.querySelectorAll(".dog-description")
+for (let i = 0; i < dogBio.length; i++){
+    dogBio[i].style.backgroundColor = 'yellow'
+}
+
